567fa46dcec6ef33b9079b01af1bb9a346d2339d
Mikael <mkvi@kth.se>
2019-09-02 12:26:34 -0700

    lab1 better example

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,6 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4])
-plt.ylabel('some label')
+meanA = [10,20]
+covA  = [[1,0], [0,1]]
+
+meanB = [30,10]
+covB  = [[10,0], [0,1]]
+
+xA, yA = np.random.multivariate_normal(meanA, covA, 100).T
+plt.plot(xA, yA, '*r')
+
+xB, yB = np.random.multivariate_normal(meanB, covB, 100).T
+plt.plot(xB, yB, '*b')
+
+plt.axis('equal')
 plt.show()
